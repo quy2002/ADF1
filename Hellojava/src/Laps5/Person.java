328aed5d77004c726310b948e9720176ccb2d8b5
quy2002 <trandinhquyzz2002@gmail.com>
2021-03-06 08:05:03 +0700

    up code

---
+++ Hellojava/src/Laps5/Person.java
@@ -0,0 +1,49 @@
+package Laps5;
+
+public class Person {
+    public String name;
+    public String gender;
+    public String birthday;
+    public String address;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void inputInfo(){
+    }
+
+    public void showInfo(){
+    }
+}
---
+++ Hellojava/src/Laps5/Student.java
@@ -0,0 +1,37 @@
+package Laps5;
+
+public class Student {
+    public int id;
+    public int mark;
+    public String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void inputInfo(){
+    }
+
+    public void showInfo(){
+    }
+}
